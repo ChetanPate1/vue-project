01f3b2f0a3094da49d61c485d1816e30ec811f60
ChetanPate1 <=>
2017-09-04 16:21:30 +0100

    component created

---
+++ src/components/FrostGlass.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="frost-glass">
+    <div className="frost" :style="{ backgroundImage: 'url('+ imgSrc +')' }" ></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FrostGlass',
+    props: {
+      imgSrc: String
+    }
+  }
+</script>
