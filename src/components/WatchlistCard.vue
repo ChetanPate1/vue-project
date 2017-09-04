8d651f4bec7fd3b1babf91c2a2fd7092e153ad55
ChetanPate1 <=>
2017-09-04 16:16:53 +0100

    displaying components props and data

--- src/components/WatchlistCard.vue
+++ src/components/WatchlistCard.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="watchlist-card">
+  <div class="watchlist-card" :style="{ backgroundImage: 'url('+ imgSrc +')' }" >
+
     <h2>{{ title }}</h2>
+    <h4>{{ subHeading }}</h4>
+    <h5>On Season {{ on.season }} Episode {{ on.episode }}</h5>
+
+    <h6>Next <small>Aired Episode</small></h6>
   </div>
 </template>
 
 <script>
   export default {
     name: 'WatchlistCard',
-    data () {
-      return {
-        title: 'Watchlist Card Component'
-      }
+    props: {
+      title: String,
+      subHeading: String,
+      on: Object,
+      imgSrc: String
     }
-}
+  }
 </script>
