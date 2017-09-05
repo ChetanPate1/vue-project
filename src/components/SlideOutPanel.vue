355e7113d2a887e16762d204493fb950ead6554c
ChetanPate1 <=>
2017-09-05 15:03:25 +0100

    created template

---
+++ src/components/SlideOutPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slide-out-panel" :class="{ 'open' : open }" >
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SlideOutPanel',
+    props: {
+      open: Boolean
+    }
+  }
+</script>
